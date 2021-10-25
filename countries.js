dc8488dcfdf951b2651161df3e81fd703ec1f8fc
anitapanczel <anita@panczel.hu>
2021-10-25 10:25:21 +0200

    format changed to be in responsive columns

--- countries.js
+++ countries.js
@@ -4,27 +4,33 @@ function loadCountry() {
 
 function showCountry(country) {
 
+
     for (let j = 0; j < country.length; j++) {
+
+        let div = document.createElement("div");
+        div.setAttribute('ID', 'column' + j);
+        div.setAttribute('class', 'col-12 col-md-6 col-lg-3');
+        document.getElementById("row").appendChild(div);
 
         console.log(country[j].name.common);
 
-        let div = document.createElement("div");
+        div = document.createElement("div");
         const name = country[j].name.official;
         div.textContent = name;
-        div.setAttribute('ID', 'name');
-        document.body.append(div);
+        div.setAttribute('class', 'name');
+        document.getElementById("column" + j).appendChild(div);
 
         div = document.createElement("div");
         const capital = country[j].capital;
         div.textContent = "Capital: " + capital;
         div.setAttribute('class', 'capital');
-        document.body.append(div);
+        document.getElementById("column" + j).appendChild(div);
 
         let img = document.createElement("img");
         const flag = country[j].flags.png;
         img.setAttribute('src', flag);
         img.setAttribute('class', 'flag');
-        document.body.append(img);
+        document.getElementById("column" + j).appendChild(img);
 
         div = document.createElement("div");
         div.textContent = "Timezone(s): ";
@@ -34,14 +40,14 @@ function showCountry(country) {
             const tz = country[j].timezones[i];
             div.textContent += ' ' + tz;
             div.setAttribute('class', 'timezone');
-            document.body.append(div);
+            document.getElementById("column" + j).appendChild(div);
         }
 
         div = document.createElement("div");
         const continents = country[1].continents[0];
         div.textContent = "Continent: " + continents;
         div.setAttribute('class', 'continents');
-        document.body.append(div);
+        document.getElementById("column" + j).appendChild(div);
     }
 }
 
